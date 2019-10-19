01f4d3fa58857ffefb900f7ff73545a2f56a1c06
adabi <amjad.dabi@gmail.com>
2019-10-19 18:26:45 -0500

    Python - added code for compiling data for any cell line into a csv file

--- Code/Python/main.py
+++ Code/Python/main.py
@@ -1,45 +1,135 @@
 import pandas as pd
-import os
 import csv
 import sys
 from collections import OrderedDict
+import pathlib
 from pathlib import Path
-#open_file = csv.reader(open(os.path.join("Data Files", "Rat Cyto 2.csv")))
+import re
+
+def find_section_in_file(file, section):
+    #Open the relevant csv file
+    open_file = csv.reader(open(file))
+
+    # Iterate throw the rows and find the row that reads 'DataType: Units'
+    starting_row_found = False
+    starting_row = 0
+    ending_row = 0
+    for index, row in enumerate(open_file):
+        if row == section:
+            starting_row = index + 1
+            starting_row_found = True
+        # Find the first empty row after the row that reads 'DataType: Median'
+        if starting_row_found:
+            if row == []:
+                ending_row = index - 1
+                break
+
+    # Check whether both starting and ending rows were found:
+    if starting_row == 0 or ending_row == 0:
+        # If not, return False
+        return None
+
+    else:
+        # If yes, load the median MFI section from the file into a dataframe
+        return pd.read_csv(file_path, error_bad_lines=False, skiprows=starting_row, nrows=ending_row-starting_row-1 , header=0)
+
+
+def find_well_in_string(string):
+    match = re.search(r'[A-H]\d{1,2}', string)
+    if match:
+        return match.group(0)
+    else:
+        return False
+
+# Grab a list of folders within the Raw directory, which will be a list of the cell lines
 p = Path('../../Data/Raw').glob('*')
-cell_lines = [x for x in p if not x.is_file()]
+cell_lines = [x.name for x in p if not x.is_file()]
+cell_lines_str = [f'{i + 1}-{cell_lines[i]}' for i in range(len(cell_lines))]
+sep = ", "
+cell_lines_output = sep.join(cell_lines_str)
 
-print(cell_lines)
+# Ask for which cell line the use wants the data compiled
+while True:
 
-'''
-# Iterate throw the rows and find the row that reads 'DataType: Units'
-starting_row_found = False
-starting_row = 0
-ending_row = 0
-for index, row in enumerate(open_file):
-    if row == ['DataType:', 'Units']:
-        starting_row = index + 1
-        starting_row_found = True
-    # Find the first empty row after the row that reads 'DataType: Median'
-    if starting_row_found:
-        if row == []:
-            ending_row = index - 1
-            break
+    chosen_cell_line = input(f'Choose the number cell line you want the data compiled for:\n '
+                             f'{cell_lines_output}:\n')
+    if chosen_cell_line in (str(list(range(1, 5)))):
+        cell_line = cell_lines[int(chosen_cell_line) - 1]
+        break
+    else:
+        print("Please choose a number corresponding to one the cell lines\n")
 
-# Check whether both starting and ending rows were found:
-if starting_row == 0 or ending_row == 0:
-    # If not, throw an error and exit
-    sys.exit("No medians section was found in the compiled data file.")
 
-else:
-    # If yes, load the median MFI section from the file into a dataframe
-    analytes_RID_df = pd.read_csv(os.path.join("Data Files", "Rat Cyto 2.csv"), error_bad_lines=False, skiprows=starting_row, nrows=ending_row-starting_row-1 , header=0)
+
+file_path = Path(f'../../Data/Raw/{cell_line}/{cell_line}.csv')
+
+analytes_RID_df = find_section_in_file(file=file_path, section=["DataType:", "Units"])
+
+if analytes_RID_df is None:
+    sys.exit("Could not find bead region IDs section in file")
 
 analytes_RID_df = analytes_RID_df.drop("Analyte:", axis=1)
-print(analytes_RID_df)
+
+analytes = analytes_RID_df.columns
 
 
-#print(analytes_RID_df[analytes_RID_df.loc["Analyte:"] == "G-CSF"])
-samples_names_df = pd.read_csv(os.path.join("Data Files", "samples.csv"))
+medians_df = find_section_in_file(file = file_path, section=["DataType:", "Median"])
+
+if medians_df is None:
+    sys.exit("Could not find medians section in file")
+
+samples_df = medians_df[['Sample', 'Location']]
+
+
+samples = (samples_df.Sample.unique())
+
+csv_files_path = Path(f'../../Data/Raw/{cell_line}/CSV/')
+csv_files = [x for x in csv_files_path.iterdir() if x.is_file() and x.suffix == '.csv']
+
+csv_files_wells = [find_well_in_string(str(x)) for x in csv_files]
+df_list = []
+
+dataframes = []
+for sample in samples:
+    data_to_save = {'Group': []}
+    event_number = {}
+    for analyte in analytes:
+        data_to_save[analyte] = pd.Series([])
+        event_number[analyte] = 0
+    sample_wells = samples_df['Location'][samples_df['Sample'] == sample].values
+
+    if len(sample_wells) > 0:
+        sample_wells = [find_well_in_string(x) for x in sample_wells]
+    else:
+        sys.exit("Could not match some samples to their repsective wells")
+
+    for well in sample_wells:
+        file = csv_files[(csv_files_wells.index(well))]
+        df_file = pd.read_csv(file,skiprows=1)
+        for analyte in analytes:
+            RID = analytes_RID_df[analyte].values[0]
+            events = df_file['RP1'][df_file['RID'] == RID].values
+            event_number[analyte] += len(events)
+
+            data_to_save[analyte] = data_to_save[analyte].append(pd.Series(events), ignore_index=True)
+
+
+    data_to_save['Group'] += [sample] * max(event_number.values())
+
+    dataframe = pd.DataFrame(data=data_to_save)
+    dataframes.append(dataframe)
+
+total_df = pd.concat(dataframes)
+
+total_df.to_csv(Path(f'../../Data/Compiled/{cell_line}.csv'))
+
+
+
+
+
+'''
+
+
 #print(samples_names_df)
 data = {"Endothelial": [True, True, True, False, False, False, False, False, False], "Microglial": [False, False, False, True, True, True, False, False, False],
         "Concentration": [0, 1, 20, 0, 1, 20, 0, 1, 20], "Wells":[[] for x in range(9)]}
